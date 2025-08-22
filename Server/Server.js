a86cd3a268e8c7eee4a3ab904c5f1b9316228e6f
ebbadullah <ebadbaloch345@email.com>
2025-08-22 22:10:02 +0500

    Updatew  cpde

--- Server/Server.js
+++ Server/Server.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use("/api", router);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 connectDB().then(() => {
   app.listen(PORT, () => {
